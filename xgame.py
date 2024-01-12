150ddab20fbf1db3845c5452d17ccd03740a2bbb
Cereal <mikedabcdefg@gmail.com>
2024-01-12 16:53:03 -0500

    Quick Update

--- xgame.py
+++ xgame.py
@@ -2,21 +2,21 @@
 while(True):
 	try:
 		rows = int(input("Enter rows: "))
-		if(rows <= 0):
+		if(rows <= 2):
 			raise ValueError()
 		break
 	except ValueError:
-		print("enter integer greater than 0")
+		print("enter integer greater than 2")
 
 
 while(True):
 	try:
 		columns = int(input("Enter columns: "))
-		if(columns <= 0):
+		if(columns <= 2):
 			raise ValueError()
 		break
 	except ValueError:
-		print("enter integer greater than 0")
+		print("enter integer greater than 2")
 
 
 while(True):
@@ -49,20 +49,76 @@ def print_game_array():
 def move(row, column):
 	game_array[row][column] = 'X'
 
+def score(row, column):
+	score_tmp = 0
+
+#	print(game_array_val(row, column) + game_array_val(row-1, column) + game_array_val(row-2, column))
+#	print(game_array_val(row-1, column) + game_array_val(row, column) + game_array_val(row+1, column))
+#	print(game_array_val(row, column) + game_array_val(row+1, column) + game_array_val(row+2, column))
+#	print(game_array_val(row, column) + game_array_val(row-1, column-1) + game_array_val(row-2, column-2))
+#	print(game_array_val(row-1, column-1) + game_array_val(row, column) + game_array_val(row+1, column+1))
+#	print(game_array_val(row, column) + game_array_val(row+1, column+1) + game_array_val(row+2, column+2))
+#	print(game_array_val(row, column) + game_array_val(row-1, column+1) + game_array_val(row-2, column+2))
+#	print(game_array_val(row-1, column+1) + game_array_val(row, column) + game_array_val(row+1, column-1))
+#	print(game_array_val(row, column) + game_array_val(row+1, column-1) + game_array_val(row+2, column-2))
+#	print(game_array_val(row, column) + game_array_val(row, column-1) + game_array_val(row, column-2))
+#	print(game_array_val(row, column-1) + game_array_val(row, column) + game_array_val(row, column+1))
+#	print(game_array_val(row, column) + game_array_val(row, column+1) + game_array_val(row, column+2))
+
+
+	if(game_array_val(row, column) + game_array_val(row-1, column) + game_array_val(row-2, column) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row-1, column) + game_array_val(row, column) + game_array_val(row+1, column) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row+1, column) + game_array_val(row+2, column) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row-1, column-1) + game_array_val(row-2, column-2) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row-1, column-1) + game_array_val(row, column) + game_array_val(row+1, column+1) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row+1, column+1) + game_array_val(row+2, column+2) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row-1, column+1) + game_array_val(row-2, column+2) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row-1, column+1) + game_array_val(row, column) + game_array_val(row+1, column-1) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row+1, column-1) + game_array_val(row+2, column-2) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row, column-1) + game_array_val(row, column-2) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column-1) + game_array_val(row, column) + game_array_val(row, column+1) == 'XXX'):
+		score_tmp += 1
+	if(game_array_val(row, column) + game_array_val(row, column+1) + game_array_val(row, column+2) == 'XXX'):
+		score_tmp += 1
+	players[player_idx][1] = players[player_idx][1] + score_tmp
+
+
+def game_array_val(row, column):
+	if(row >= 0 and row < rows and column >=0 and column < columns):
+		return game_array[row][column]
+	else:
+		return ''
+
 
 player_idx = 0
+moves = 0
 
 
 while(True):
+	if(moves == rows*columns):
+		print("Game Over!")
+		for player in sorted(players, key=lambda x: x[1], reverse=True):
+			print(player[0] + ' ' + str(player[1]))
+		break
 	user_input = input("xgame>").split()
 	if(len(user_input) == 0):
 		pass
 	elif(user_input[0] == 'p'):
 		for idx,player in enumerate(players):
-			str = player[0] + ' ' + str(player[1])
+			player_str = player[0] + ' ' + str(player[1])
 			if(player_idx == idx):
-				str = str + ' (Your Turn)'
-			print(str)
+				player_str = player_str + ' (Your Turn)'
+			print(player_str)
 	elif(user_input[0] == 'b'):
 		print_game_array()
 	elif(user_input[0] == 'm'):
@@ -92,7 +148,11 @@ while(True):
 				move_error = True
 		if(not move_error):
 			game_array[user_input[1]][user_input[2]] = 'X'
+			score(user_input[1], user_input[2])
 			player_idx = (player_idx + 1) % num_players
+			moves = moves + 1
+	elif(user_input[0] == '?' or user_input[0] == 'h' or user_input[0] == 'help'):
+		print(f"?/h/help - prints help\nb - prints board\np - prints player info\nm <row> <column> - puts x")
 	else:
 		print("not a command")
 
